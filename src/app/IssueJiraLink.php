89133b42b79d40811df0979fc50da22467ffdcae
Diego Alvarez <diegoalvarez17@gmail.com>
2019-03-20 23:20:09 -0300

    Added relations between tasks

---
+++ src/app/IssueJiraLink.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use JiraRestApi\IssueLink\IssueLink;
+use JiraRestApi\IssueLink\IssueLinkService;
+
+class IssueJiraLink
+{
+
+    public function linkIssue($origin, $destiny, $relation = 'relates to')
+    {
+        $il = new IssueLink();
+
+        $il->setInwardIssue($origin)
+            ->setOutwardIssue($destiny)
+            ->setLinkTypeName($relation);
+
+        $ils = new IssueLinkService();
+
+        // $ret = $ils->addIssueLink($il);
+    }
+}
